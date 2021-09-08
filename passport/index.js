3bf631c3fc397a873a09453b48f750f36d311d22
syh <dbsgur6896@naver.com>
2021-09-08 22:29:01 +0900

    0908

---
+++ passport/index.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+    //로그인 시 실행
+    passport.serializeUser((user, done) => {
+        //done 함수의 첫 번째 인수는 에러 발생시 사용
+        done(null, user.id);
+    });
+
+    //매 요청 시 실행
+    passport.deserializeUser((id, done) => {
+        User.findOne({ where: { id } })
+            .then(user => done(null, user)) //req.user에 저장
+            .catch(err => done(err));
+    });
+
+    local();
+    kakao();
+}
